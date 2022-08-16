5259b9ed209399d0c0bb83188970c68625a80561
Peter Kraus <fuudstamps@gmail.com>
2022-08-16 16:06:14 -0400

    erb < slim

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+// https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+
+// TODO: Add colors
+// TODO: Add fonts
+// TODO: Add font sizes
+// TODO: Add font weights
+// TODO: ADd h1-h6
 
 module.exports = {
   content: [
